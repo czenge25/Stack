58bda22a0848624bafbdf3c77591c3e7159dfa00
czenge25 <czenge25@kentdenver.org>
2023-10-24 12:21:34 -0600

    Finished isEmpty method

--- Stack/src/Stack.java
+++ Stack/src/Stack.java
@@ -20,7 +20,11 @@ public class Stack {
     }
 
     public boolean isEmpty() {
-
+        if (size == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public int size() {
