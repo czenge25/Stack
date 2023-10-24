fb29a8acdd6297dfa81f3319383786d9b2ffec06
czenge25 <czenge25@kentdenver.org>
2023-10-24 12:18:57 -0600

    Created outline of Stack Class

---
+++ Stack/src/Stack.java
@@ -0,0 +1,30 @@
+public class Stack {
+
+    private int size;
+    private StackNode top;
+
+    public Stack() {
+
+    }
+
+    public void push(StackNode n) {
+
+    }
+
+    public StackNode pop() {
+
+    }
+
+    public StackNode peek() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public int size() {
+
+    }
+
+}
