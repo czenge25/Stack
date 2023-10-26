3faaffc779b5b49f416203d7b7391b9321e425a7
czenge25 <czenge25@kentdenver.org>
2023-10-26 09:57:44 -0600

    Finished testing class in Main

--- Stack/src/Main.java
+++ Stack/src/Main.java
@@ -1,5 +1,41 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Random r = new Random();
+        Stack s = new Stack();
+
+        for (int i = 0; i < 10; i++) {
+            s.push(r.nextInt(100));
+            System.out.println(s.peek());
+        }
+        System.out.println("Size: " + s.size());
+
+        System.out.println("---");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Size: " + s.size());
+            System.out.println(s.peek());
+            s.pop();
+        }
+        System.out.println("Size: " + s.size());
+
+        System.out.println("---");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Size: " + s.size());
+            System.out.println(s.peek());
+            s.pop();
+        }
+        System.out.println("Size: " + s.size());
+
+        System.out.println("---");
+
+        System.out.println("Stack is empty: " + s.isEmpty());
+        System.out.println("Size: " + s.size());
+
+        System.out.println("---");
+
     }
 }
